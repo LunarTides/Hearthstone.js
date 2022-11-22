33d5440f9482a096fb309f0ceca0390f026d8c35
IsakSkole123 <sangedal@elev.rogfk.no>
2022-11-22 12:50:06 +0100

    Added known bugs

---
+++ known_bugs.py
@@ -0,0 +1,12 @@
+'''
+Levels:
+    Critical
+    Severe
+    High
+    Medium
+    Low
+
+    
+The ai somehow duplicates their minions (Severe)
+When a minion gets a keyword added to them using something like adapt, the keyword gets added to the blueprint, making every new instance of the card have that keyword (High)
+'''
