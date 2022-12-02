f8e73ae029b7998a1d3917c8a33224ccc70fc317
IsakSkole123 <sangedal@elev.rogfk.no>
2022-12-02 12:20:17 +0100

    Small index reformatting

--- src/index.js
+++ src/index.js
@@ -1,29 +1,27 @@
 const { Game } = require("./game");
 const { Player } = require("./player");
-const config = require("../config.json");
+const config = require("../config");
 
-let player1 = new Player("Player 1");
-let player2 = new Player("Player 2");
-let game = new Game(player1, player2, config);
+const p1 = new Player("Player 1");
+const p2 = new Player("Player 2");
+const game = new Game(p1, p2, config);
 
 if (!config.debug) {
-    const input = require("readline-sync").question; // Python input
-
     game.interact.printName();
 
-    player1.name = input("Player 1, what is your name? ");
-    player2.name = input("Player 2, what is your name? ");
+    p1.name = game.input("Player 1, what is your name? ");
+    p2.name = game.input("Player 2, what is your name? ");
 }
 
 game.functions.importCards(__dirname + '/../cards');
 
 // Ask the players for deck codes.
-game.interact.deckCode(player1);
-game.interact.deckCode(player2);
+game.interact.deckCode(p1);
+game.interact.deckCode(p2);
 
 game.startGame();
 
-game.interact.mulligan(player1);
-game.interact.mulligan(player2);
+game.interact.mulligan(p1);
+game.interact.mulligan(p2);
 
 while (true) game.interact.doTurn();
