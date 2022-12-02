e3d498b01124863f5552ac45049f0d6f4e3cc51d
IsakSkole123 <sangedal@elev.rogfk.no>
2022-12-02 12:41:32 +0100

    Moved code in src/index to /index

--- index.js
+++ index.js
@@ -3,4 +3,30 @@ Hearthstone.js - Hearthstone but console based.
 Copyright (C) 2022  Keatpole
 */
 
-require("./src/index");
+const { Game } = require("./src/game");
+const { Player } = require("./src/player");
+const config = require("./config");
+
+const p1 = new Player("Player 1");
+const p2 = new Player("Player 2");
+const game = new Game(p1, p2, config);
+
+if (!config.debug) {
+    game.interact.printName();
+
+    p1.name = game.input("Player 1, what is your name? ");
+    p2.name = game.input("Player 2, what is your name? ");
+}
+
+game.functions.importCards(__dirname + '/cards');
+
+// Ask the players for deck codes.
+game.interact.deckCode(p1);
+game.interact.deckCode(p2);
+
+game.startGame();
+
+game.interact.mulligan(p1);
+game.interact.mulligan(p2);
+
+while (true) game.interact.doTurn();
