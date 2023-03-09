27e2ccb4fae1af4c0c3e6bcce39162bcf2083f00
IsakSkole123 <sangedal@elev.rogfk.no>
2023-03-09 11:27:21 +0100

    Identity Theft

---
+++ cards/Classes/Priest/Spells/3 Cost/identity_theft.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: "Identity Theft",
+    desc: "&BDiscover&R a copy of a card from your opponent's hand and deck.",
+    mana: 3,
+    class: "Priest",
+    rarity: "Common",
+    set: "Murder at Castle Nathria",
+    spellClass: "Shadow",
+    id: 249,
+
+    cast(plr, game, self) {
+        let list = plr.getOpponent().hand;
+        game.interact.discover("Discover a copy of a card from your opponent's hand.", list);
+
+        list = plr.getOpponent().deck;
+        game.interact.discover("Discover a copy of a card from your opponent's deck.", list);
+    }
+}
