53cbd0517620e96aa8008624498d87dc3019efe5
IsakSkole123 <sangedal@elev.rogfk.no>
2023-03-09 08:55:25 +0100

    Mistake

---
+++ cards/Classes/Neutral/Minions/1 Cost/mistake.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: "Mistake",
+    stats: [1, 3],
+    desc: "This has all minion types.",
+    mana: 1,
+    tribe: "All",
+    class: "Neutral",
+    rarity: "Epic",
+    set: "Return to Naxxramas",
+    id: 231,
+}
