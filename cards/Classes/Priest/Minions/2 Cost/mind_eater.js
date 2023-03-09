cc62f8e1a91d58af4864ff4ea282110b15ed918e
IsakSkole123 <sangedal@elev.rogfk.no>
2023-03-09 09:25:59 +0100

    Mind Eater

---
+++ cards/Classes/Priest/Minions/2 Cost/mind_eater.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: "Mind Eater",
+    stats: [3, 2],
+    desc: "&BDeathrattle:&R Add a copy of a card in your opponent's deck to your hand.",
+    mana: 2,
+    tribe: "Undead",
+    class: "Priest",
+    rarity: "Common",
+    set: "March of the Lich King",
+    id: 235,
+
+    deathrattle(plr, game, self) {
+        let list = plr.getOpponent().deck;
+        let card = game.functions.randList(list);
+
+        plr.addToHand(card);
+    }
+}
