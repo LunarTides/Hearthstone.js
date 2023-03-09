b624b8ea112f06ac92b34cd7caba9c0efe42a9a7
IsakSkole123 <sangedal@elev.rogfk.no>
2023-03-09 09:10:48 +0100

    Sir Finley, Sea Guide

---
+++ cards/Classes/Neutral/Minions/1 Cost/sir_finley_sea_guide.js
@@ -0,0 +1,23 @@
+module.exports = {
+    name: "Sir Finley Sea Guide",
+    displayName: "Sir Finley, Sea Guide",
+    stats: [1, 3],
+    desc: "&BBattlecry:&R Swap your hand with the bottom of your deck.",
+    mana: 1,
+    tribe: "Murloc",
+    class: "Neutral",
+    rarity: "Legendary",
+    set: "Voyage to the Sunken City",
+    id: 233,
+
+    battlecry(plr, game, self) {
+        let bottom_of_deck = plr.deck.splice(0, plr.hand.length);
+
+        // Add player's hand to the bottom of their deck.
+        plr.hand.forEach(c => {
+            plr.deck.unshift(c);
+        });
+
+        plr.hand = bottom_of_deck;
+    }
+}
