2453468b5718ef7809e22f9bd40265272abb9225
IsakSkole123 <sangedal@elev.rogfk.no>
2023-03-09 09:33:03 +0100

    Serena Bloodfeather

---
+++ cards/Classes/Priest/Minions/2 Cost/serena_bloodfeather.js
@@ -0,0 +1,22 @@
+module.exports = {
+    name: "Serena Bloodfeather",
+    stats: [1, 1],
+    desc: "&BBattlecry:&R Choose an enemy minion. Steal Attack and Health from it until this has more.",
+    mana: 2,
+    tribe: "None",
+    class: "Priest",
+    rarity: "Legendary",
+    set: "Forged in the Barrens",
+    id: 236,
+
+    battlecry(plr, game, self) {
+        let target = game.interact.selectTarget("Choose an enemy minion. Steal Attack and Health from it until this has more.", false, "enemy", "minion");
+        if (!target) return -1;
+
+        do {
+            // Steal attack and health
+            self.addStats(1, 1);
+            target.remStats(1, 1);
+        } while (target.getAttack() > self.getAttack() || target.getHealth() > self.getHealth());
+    }
+}
