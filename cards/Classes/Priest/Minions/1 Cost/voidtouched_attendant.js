2273f22fba3cb54ea21fab02ef2da9f84190ecea
IsakSkole123 <sangedal@elev.rogfk.no>
2023-03-09 09:23:30 +0100

    Voidtouched Attendant

---
+++ cards/Classes/Priest/Minions/1 Cost/voidtouched_attendant.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: "Voidtouched Attendant",
+    stats: [1, 3],
+    desc: "Both heroes take one extra damage from all sources.",
+    mana: 1,
+    tribe: "None",
+    class: "Priest",
+    rarity: "Epic",
+    set: "United in Stormwind",
+    id: 234,
+
+    passive(plr, game, self, key, val) {
+        if (key != "damageTaken") return;
+
+        let [player, amount] = val;
+
+        player.remHealth(1, false);
+    }
+}
