1793edaee49bbfed126f6036039b6a7b9ce1610c
IsakSkole123 <sangedal@elev.rogfk.no>
2023-03-09 09:02:13 +0100

    Shadowcloth Needle

---
+++ cards/Classes/Priest/Weapons/1 Cost/shadowcloth_needle.js
@@ -0,0 +1,22 @@
+module.exports = {
+    name: "Shadowcloth Needle",
+    stats: [0, 3],
+    desc: "After you cast a Shadow spell, deal 1 damage to all enemies. Lose 1 Durability.",
+    mana: 1,
+    class: "Priest",
+    rarity: "Rare",
+    set: "United in Stormwind",
+    id: 232,
+
+    passive(plr, game, self, key, val) {
+        if (key != "cardsPlayed" || game.player != plr) return;
+        if (val.type != "Spell" || !val.spellClass || !val.spellClass.includes("Shadow")) return;
+
+        // The player cast a Shadow spell
+        game.functions.doPlayerTargets(plr.getOpponent(), (t) => {
+            game.attack(1, t);
+        });
+
+        self.remStats(0, 1);
+    }
+}
