697d7423670ae76c295af4e438eac72fc69fe22c
IsakSkole123 <sangedal@elev.rogfk.no>
2023-05-16 12:21:51 +0200

    Reworked adapt

--- src/functions.js
+++ src/functions.js
@@ -1170,112 +1170,9 @@ ${aiHistory}
 
     // Keyword stuff
     adapt(minion, prompt = "Choose One:", _values = []) {
-        /**
-         * Asks the user a "prompt" and show 3 choices for the player to choose, and do something to the minion based on the choice
-         * 
-         * @param {Card} minion The minion to adapt
-         * @param {string} prompt [default="Choose One:"] The prompt to ask the user
-         * 
-         * @returns {string} The name of the adapt chosen. See the first values of possible_cards
-         */
+        const ADAPT = new game.Card("Adapt Helper", game.player);
 
-        game.interact.printAll(game.player);
-
-        let possible_cards = [
-            ["Crackling Shield", "Divine Shield"],
-            ["Flaming Claws", "+3 Attack"],
-            ["Living Spores", "Deathrattle: Summon two 1/1 Plants."],
-            ["Lightning Speed", "Windfury"],
-            ["Liquid Membrane", "Can't be targeted by spells or Hero Powers."],
-            ["Massive", "Taunt"],
-            ["Volcanic Might", "+1/+1"],
-            ["Rocky Carapace", "+3 Health"],
-            ["Shrouding Mist", "Stealth until your next turn."],
-            ["Poison Spit", "Poisonous"]
-        ];
-        let values = _values;
-
-        if (values.length == 0) {
-            for (let i = 0; i < 3; i++) {
-                let c = game.functions.randList(possible_cards);
-
-                values.push(c);
-                this.remove(possible_cards, c);
-            }
-        }
-
-        let p = `\n${prompt}\n[\n`;
-
-        values.forEach((v, i) => {
-            // Check for a TypeError and ignore it
-            try {
-                p += `${i + 1}: ${v[0]}; ${v[1]},\n`;
-            } catch (e) {}
-        });
-
-        p = p.slice(0, -2);
-        p += "\n] ";
-
-        let choice = game.input(p);
-        if (!parseInt(choice)) {
-            game.input("Invalid choice!\n".red);
-            return this.adapt(minion, prompt, values);
-        }
-
-        if (parseInt(choice) > 3) return this.adapt(minion, prompt, values);
-
-        choice = values[parseInt(choice) - 1][0];
-
-        switch (choice) {
-            case "Crackling Shield":
-                minion.addKeyword("Divine Shield");
-
-                break;
-            case "Flaming Claws":
-                minion.addStats(3, 0);
-
-                break;
-            case "Living Spores":
-                minion.addDeathrattle((plr, game) => {
-                    game.summonMinion(new game.Card("Plant"), plr);
-                    game.summonMinion(new game.Card("Plant"), plr);
-                });
-
-                break;
-            case "Lightning Speed":
-                minion.addKeyword("Windfury");
-
-                break;
-            case "Liquid Membrane":
-                minion.addKeyword("Elusive");
-
-                break;
-            case "Massive":
-                minion.addKeyword("Taunt");
-
-                break;
-            case "Volcanic Might":
-                minion.addStats(1, 1);
-
-                break;
-            case "Rocky Carapace":
-                minion.addStats(0, 3);
-
-                break;
-            case "Shrouding Mist":
-                minion.addKeyword("Stealth");
-                minion.setStealthDuration(1);
-
-                break;
-            case "Poison Spit":
-                minion.addKeyword("Poisonous");
-
-                break;
-            default:
-                break;
-        }
-
-        return choice;
+        ADAPT.activate("adapt", minion, prompt, _values);
     }
     invoke(plr) {
         /**
@@ -1289,8 +1186,9 @@ ${aiHistory}
         // Filter all cards in "plr"'s deck with a name that starts with "Galakrond, the "
         
         // --- REMOVE FOR DEBUGGING ---
-        let cards = plr.deck.filter(c => c.displayName.startsWith("Galakrond, the "));
-        if (cards.length <= 0) return;
+        let deck_cards = plr.deck.filter(c => c.displayName.startsWith("Galakrond, the "));
+        let hand_cards = plr.hand.filter(c => c.displayName.startsWith("Galakrond, the "));
+        if ((deck_cards.length + hand_cards.length <= 0) || !plr.hero.displayName.startsWith("Galakrond, the ")) return;
         // ----------------------------
 
         switch (plr.heroClass) {
