2c9d8057532c4274d3910c5d16e3a4f44e3c88c7
IsakSkole123 <sangedal@elev.rogfk.no>
2023-08-24 09:59:23 -0700

    Fixed tests

--- src/game.js
+++ src/game.js
@@ -188,7 +188,7 @@ class EventManager {
      * @returns {boolean} Success
      */
     broadcast(key, val, plr, updateHistory = true) {
-        if (!(plr instanceof Player) || plr.id === -1) return false;
+        if (plr.classType !== "Player" || plr.id === -1) return false;
         this.tick(key, val);
 
         if (updateHistory) this.addHistory(key, val, plr);
