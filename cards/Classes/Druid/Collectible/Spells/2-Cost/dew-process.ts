6421f02e4050280e4cb00493526f1266599374a3
LunarTides <31688109+LunarTides@users.noreply.github.com>
2023-10-16 13:15:58 +0200

    card: dew process

---
+++ cards/Classes/Druid/Collectible/Spells/2-Cost/dew-process.ts
@@ -0,0 +1,28 @@
+// Created by the Vanilla Card Creator
+
+import assert from 'node:assert';
+import {type Blueprint} from '@Game/types.js';
+
+export const blueprint: Blueprint = {
+    name: 'Dew Process',
+    text: 'For the rest of the game, players draw an extra card at the start of their turn.',
+    cost: 2,
+    type: 'Spell',
+    spellSchool: 'Nature',
+    classes: ['Druid'],
+    rarity: 'Rare',
+    id: 87,
+
+    cast(plr, self) {
+        // For the rest of the game, players draw an extra card at the start of their turn.
+        game.functions.event.addListener('StartTurn', (_unknownValue, eventPlayer) => {
+            eventPlayer.drawCard();
+            return true;
+        }, -1);
+    },
+
+    test(plr, self) {
+        // Unit testing
+        assert(false);
+    },
+};
