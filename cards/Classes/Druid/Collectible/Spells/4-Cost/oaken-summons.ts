45bedaaf4dd493f38685656d2b182bc94f67a399
LunarTides <31688109+LunarTides@users.noreply.github.com>
2023-10-16 13:16:38 +0200

    card: oaken summons

---
+++ cards/Classes/Druid/Collectible/Spells/4-Cost/oaken-summons.ts
@@ -0,0 +1,28 @@
+// Created by the Vanilla Card Creator
+
+import assert from 'node:assert';
+import {type Blueprint} from '@Game/types.js';
+
+export const blueprint: Blueprint = {
+    name: 'Oaken Summons',
+    text: 'Gain 6 Armor. <b>Recruit</b> a minion that costs (4) or less.',
+    cost: 4,
+    type: 'Spell',
+    spellSchool: 'Nature',
+    classes: ['Druid'],
+    rarity: 'Common',
+    id: 83,
+
+    cast(plr, self) {
+        // Gain 6 Armor. Recruit a minion that costs (4) or less.
+        plr.addArmor(6);
+
+        const list = plr.deck.filter(card => card.cost <= 4);
+        plr.recruit(list);
+    },
+
+    test(plr, self) {
+        // Unit testing
+        assert(false);
+    },
+};
