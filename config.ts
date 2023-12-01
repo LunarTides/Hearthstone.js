2f2f8fcda0d68417ac3a608b6b6dde6747284762
LunarTides <31688109+LunarTides@users.noreply.github.com>
2023-12-01 16:32:27 +0100

    chore: bump version to 2.0.0-beta.7

--- config.ts
+++ config.ts
@@ -229,7 +229,7 @@ export const config: GameConfig = {
         branch: 'beta',
 
         // The build number of the game.
-        build: 6,
+        build: 7,
 
         // The text that is displayed along this specific version/branch combination.
         versionText: 'Make the project use typescript instead of javascript',
