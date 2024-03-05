LunarTides/Hearthstone.js

commit f4a3b6841b57562e8758de047cdd6945e83949aa

refactor!: replace player.drawCard with player.drawCards

src/core/player.ts
@@ -2,7 +2,7 @@
  * Player
  * @module Player
  */
-import { Card, type Ai } from '@Game/internal.js';
+import { type Card, type Ai } from '@Game/internal.js';
 import { type CardClass, type CardType, type EventKey, type QuestCallback, type QuestType, type Target } from '@Game/types.js';
 
 export class Player {
@@ -673,45 +673,6 @@ export class Player {
     }
 
     /**
-     * Draws the card from the top of this player's deck.
-     * Broadcasts the `DrawCard` event
-     *
-     * @returns The card drawn | The amount of fatigue the player was dealt
-     */
-    drawCard(): Card | number {
-        const deckLength = this.deck.length;
-
-        /**
-         * The card to draw
-         */
-        const card = this.deck.pop();
-
-        if (deckLength <= 0 || !card) {
-            this.fatigue++;
-
-            this.remHealth(this.fatigue);
-            return this.fatigue;
-        }
-
-        game.event.broadcast('DrawCard', card, this);
-
-        // Cast on draw
-        if (card.type === 'Spell' && card.hasKeyword('Cast On Draw') && card.activate('cast')) {
-            return this.drawCard();
-        }
-
-        // Summon on draw
-        if (card.hasKeyword('Summon On Draw') && card.canBeOnBoard()) {
-            this.summon(card);
-            return this.drawCard();
-        }
-
-        game.functions.event.withSuppressed('AddCardToHand', () => this.addToHand(card));
-
-        return card;
-    }
-
-    /**
      * Draws `amount` cards from this player's deck.
      * Broadcasts the `DrawCard` event for each card drawn
      *
@@ -721,13 +682,39 @@ export class Player {
     drawCards(amount: number): Card[] {
         const cards: Card[] = [];
 
-        for (let i = 0; i < amount; i++) {
-            const card = this.drawCard();
+        const unsuppress = game.functions.event.suppress('AddCardToHand');
 
-            if (card instanceof Card) {
-                cards.push(card);
+        for (let i = 0; i < amount; i++) {
+            const card = this.deck.pop();
+
+            // Fatigue
+            if (this.deck.length <= 0 || !card) {
+                this.fatigue++;
+
+                this.remHealth(this.fatigue);
+                continue;
             }
+
+            // Cast on draw
+            if (card.type === 'Spell' && card.hasKeyword('Cast On Draw') && card.activate('cast')) {
+                amount += 1;
+                continue;
+            }
+
+            // Summon on draw
+            if (card.hasKeyword('Summon On Draw') && card.canBeOnBoard()) {
+                this.summon(card);
+
+                amount += 1;
+                continue;
+            }
+
+            this.addToHand(card);
+            game.event.broadcast('DrawCard', card, this);
+            cards.push(card);
         }
+
+        unsuppress();
 
         return cards;
     }
@@ -1001,7 +988,7 @@ export class Player {
 
             game.functions.util.remove(mulligan, card);
 
-            game.functions.event.withSuppressed('DrawCard', () => this.drawCard());
+            game.functions.event.withSuppressed('DrawCard', () => this.drawCards(1));
             game.functions.event.withSuppressed('AddCardToDeck', () => this.shuffleIntoDeck(card));
             game.functions.event.withSuppressed('DiscardCard', () => card.discard());
 

cards/Classes/Druid/Collectible/Heroes/5-Cost/89-wildheart-guff.ts
@@ -20,7 +20,7 @@ export const blueprint: Blueprint = {
         // Set your maximum Mana to 20. Gain an empty Mana Crystal. Draw a card.
         plr.maxMana = 20;
         plr.addEmptyMana(1);
-        plr.drawCard();
+        plr.drawCards(1);
     },
 
     test(plr, self) {

cards/Classes/Druid/Collectible/Spells/0-Cost/93-aquatic-form.ts
@@ -22,7 +22,7 @@ export const blueprint: Blueprint = {
             return;
         }
 
-        plr.drawCard();
+        plr.drawCards(1);
     },
 
     test(plr, self) {

cards/Classes/Druid/Collectible/Spells/2-Cost/91-dew-process.ts
@@ -18,7 +18,7 @@ export const blueprint: Blueprint = {
     cast(plr, self) {
         // For the rest of the game, players draw an extra card at the start of their turn.
         game.functions.event.addListener('StartTurn', (_unknownValue, eventPlayer) => {
-            eventPlayer.drawCard();
+            eventPlayer.drawCards(1);
             return true;
         }, -1);
     },

cards/Classes/Druid/Collectible/Spells/2-Cost/92-invigorate.ts
@@ -22,7 +22,7 @@ export const blueprint: Blueprint = {
             plr.addEmptyMana(1);
         }], ['Draw a card', () => {
             // Draw a card
-            plr.drawCard();
+            plr.drawCards(1);
         }]);
     },
 

cards/Classes/Druid/Collectible/Spells/3-Cost/94-frost-lotus-seedling.ts
@@ -44,7 +44,7 @@ export const blueprint: Blueprint = {
         if (blossomed) {
             plr.drawCards(2);
         } else {
-            plr.drawCard();
+            plr.drawCards(1);
         }
 
         plr.addArmor(blossomed ? 10 : 5);

cards/Classes/Druid/Collectible/Spells/4-Cost/86-branching-paths.ts
@@ -19,7 +19,7 @@ export const blueprint: Blueprint = {
         // Choose Twice - Draw a card; Give your minions +1 Attack; Gain 6 Armor.
         game.interact.chooseOne(2, ['Draw a card', () => {
             // Draw a card
-            plr.drawCard();
+            plr.drawCards(1);
         }], ['Give your minions +1 Attack', () => {
             // Give your minions +1 Attack
             for (const card of plr.board) {

cards/Classes/Druid/Uncollectible/Heropowers/2-Cost/113-nurture.ts
@@ -17,7 +17,7 @@ export const blueprint: Blueprint = {
         // Choose One - Draw a card; or Gain an empty Mana Crystal.
         game.interact.chooseOne(1, ['Draw a card', () => {
             // Draw a card
-            plr.drawCard();
+            plr.drawCards(1);
         }], ['Gain an empty Mana Crystal', () => {
             // Gain an empty ManaCrystal
             plr.addEmptyMana(1);

commit 0e04aa180dd9e5ce7a0fc743fe5e4f028cb622a9

fix: fix fatigue bug
Regression from f4a3b684

src/core/player.ts
@@ -682,10 +682,12 @@ export class Player {
         const unsuppress = game.functions.event.suppress('AddCardToHand');
 
         for (let i = 0; i < amount; i++) {
+            // We need the `deckLength` variable since pop may change the length of the deck
+            const deckLength = this.deck.length;
             const card = this.deck.pop();
 
             // Fatigue
-            if (this.deck.length <= 0 || !card) {
+            if (deckLength <= 0 || !card) {
                 this.fatigue++;
 
                 this.remHealth(this.fatigue);