d73145908e700560d85c0bff629dbf420a417ed1
LunarTides <31688109+LunarTides@users.noreply.github.com>
2024-06-03 13:01:50 +0200

    refactor: make propfinder format data in a more useful way

--- scripts/vanilla/propfinder.ts
+++ scripts/vanilla/propfinder.ts
@@ -3,6 +3,7 @@
  */
 
 import process from 'node:process';
+import date from 'date-and-time';
 import { createGame } from '@Game/internal.js';
 import { type VanillaCard } from '@Game/types.js';
 
@@ -78,19 +79,100 @@ function main(): void {
         }
     }
 
-    console.warn('Done!');
+    const now = new Date();
+    const dateString = date.format(now, 'DD/MM/YYYY');
 
-    console.log('<b>PROPS:</b>');
-    console.log(props);
-    console.log('<b>STORED:</b>');
+    console.log(`// Last Updated: ${dateString} (DD/MM/YYYY)`);
+    console.log('// Tested with: https://hearthstonejson.com');
 
     for (const object of Object.entries(stored)) {
         let [key, value] = object;
         value = value.sort((a, b) => b[1] - a[1]);
 
-        console.log('%s:', key);
-        console.log(value);
+        console.log('\nexport type %s =', key.slice(0, 1).toUpperCase() + key.slice(1));
+        for (let i = 0; i < value.length; i++) {
+            const v = value[i];
+
+            if (i >= value.length - 1) {
+                console.log(`// ${v[1]} Cards\n| '${v[0]}';`);
+            } else {
+                console.log(`// ${v[1]} Cards\n| '${v[0]}'`);
+            }
+        }
     }
+
+    console.log(`
+/**
+ * Hearthstone's card blueprint.
+ */
+export type Card = {
+    id: string;
+    dbfId: number;
+    name: string;
+    text?: string;
+    flavor?: string;
+    artist?: string;
+    cardClass?: CardClass;
+    // Collectible is either set to true, or not set at all.
+    collectible?: boolean;
+    cost?: number;
+    mechanics?: Mechanics[];
+    rarity?: Rarity;
+    set: Set;
+    race?: Race;
+    races?: Race[];
+    type: Type;
+    spellSchool?: SpellSchool;
+    durability?: number;
+    faction?: Faction;
+    // I am not sure what this is, but it is either set to true, or not set at all.
+    // It might have something to do with legendaries. Maybe making sure that only 1 can be in a deck at a time.
+    elite?: boolean;
+    attack?: number;
+    health?: number;
+
+    howToEarn?: string;
+    // All props below this line was found by a script (vcpropfinder)
+    classes?: CardClass[];
+    heroPowerDbfId?: number;
+    referencesTags?: Mechanics[];
+    targetingArrowText?: string;
+    overload?: number;
+    spellDamage?: number;
+    collectionText?: string;
+    // Has Diamond Skin is either set to true, or not set at all.
+    hasDiamondSkin?: boolean;
+    howToEarnGolden?: string;
+    armor?: number;
+    multiClassGroup?: MultiClassGroup;
+    // Is mini set is either set to true, or not set at all.
+    isMiniSet?: boolean;
+    // This seems like it is the id (not dbfId) of the reward card.
+    questReward?: string;
+
+    // Likely part of other gamemodes.
+    mercenariesRole?: string;
+    mercenariesAbilityCooldown?: number;
+    techLevel?: number;
+    // Hide Cost is either set to true, or not set at all.
+    hideCost?: boolean;
+    // Hide Stats is either set to true, or not set at all.
+    hideStats?: boolean;
+    // Is battlegrounds pool minion is either set to true, or not set at all.
+    isBattlegroundsPoolMinion?: boolean;
+    battlegroundsPremiumDbfId?: number;
+    battlegroundsNormalDbfId?: number;
+    battlegroundsBuddyDbfId?: number;
+    // Battlegrounds hero is either set to true, or not set at all.
+    battlegroundsHero?: boolean;
+    // Is battlegrounds buddy is either set to true, or not set at all.
+    isBattlegroundsBuddy?: boolean;
+    battlegroundsSkinParentId?: number;
+    battlegroundsDarkmoonPrizeTurn?: number;
+    countAsCopyOfDbfId?: number;
+    puzzleType?: number;
+};
+`);
 }
 
 main();
