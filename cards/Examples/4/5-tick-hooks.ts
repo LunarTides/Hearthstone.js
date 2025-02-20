e7a7728be9d7da6ebca36cc2dcd1a41c935ccc38
LunarTides <31688109+LunarTides@users.noreply.github.com>
2025-02-20 11:53:19 +0100

    Change tick hook example card
    Closes #373

--- cards/Examples/4/5-tick-hooks.ts
+++ cards/Examples/4/5-tick-hooks.ts
@@ -1,35 +1,33 @@
 // Created by Hand
 
+import assert from "node:assert";
+import { Card } from "@Game/card.js";
 import {
+	Ability,
 	type Blueprint,
 	Class,
-	MinionTribe,
+	Event,
+	GamePlayCardReturn,
 	Rarity,
+	SpellSchool,
 	Type,
 } from "@Game/types.js";
 
 export const blueprint: Blueprint = {
 	name: "Tick Hook Example",
-	text: "Your cards cost (1) less.",
-	cost: 1,
-	type: Type.Minion,
+	text: "Fill up your mana. For the rest of the game, your mana never decreases.",
+	cost: 0,
+	type: Type.Spell,
 	classes: [Class.Neutral],
 	rarity: Rarity.Free,
 	collectible: false,
 	tags: [],
 	id: 59,
 
-	attack: 1,
-	health: 1,
-	tribes: [MinionTribe.None],
+	spellSchools: [SpellSchool.None],
 
-	async create(owner, self) {
-		// Initialize storage
-		self.storage.unhooks = [];
-	},
-
-	async battlecry(owner, self) {
-		// Your cards cost (1) less.
+	async cast(owner, self) {
+		// Fill up your mana. For the rest of the game, your mana never decreases.
 
 		/*
 		 * Ticks are called more often than passives.
@@ -43,53 +41,38 @@ export const blueprint: Blueprint = {
 		 * You are given the key and value of the event.
 		 * I don't think you will need them for tick hooks, since they are not supposed to be dependent on events, but you are free to use them if you want.
 		 */
-		const unhook = game.event.hookToTick(async () => {
-			for (const card of owner.hand) {
-				if (card.enchantmentExists("-1 cost", self)) {
-					continue;
-				}
 
-				card.addEnchantment("-1 cost", self);
-			}
+		/*
+		 * Gain max mana every tick.
+		 * This lasts for the rest of the game, since we don't unhook it.
+		 * This is *technically* not what the text says,
+		 * but the user won't notice the difference since the mana gets refilled too quickly.
+		 */
+		const unhook = game.event.hookToTick(async () => {
+			owner.addMana(owner.maxMana);
 		});
 
 		// If you want to use an event, do this:
 		// const unhook = game.event.hookToTick<Event.PlayCard>(async (key, value) => {});
-
-		/*
-		 * Store the unhook to be used later in the `remove` ability. This is the only supported way to transfer information between abilities.
-		 * You can store anything in a card, and it shouldn't be messed with by other cards / the game.
-		 * Speaking of, you should never mess with another card's storage since it can cause unexpected behavior.
-		 */
-		self.storage.unhooks.push(unhook);
-	},
-
-	// Unhook from the tick when the card is removed
-	async remove(owner, self) {
-		/*
-		 * TODO: Change this example card to use a better example. I'm sure there is a card that uses tick hooks in a better way that we can copy.
-		 * This is a bad example, since this is what the `tick` ability is supposed to do anyway.
-		 * #373
-		 */
-
-		/*
-		 * Unhook from all ticks that the card is hooked to.
-		 * It is important to unhook before removing the enchantments, since removing the enchantments could possibly cause a tick, which would add the enchantments back.
-		 */
-		if (Array.isArray(self.storage.unhooks)) {
-			for (const unhook of self.storage.unhooks) {
-				(unhook as () => void)();
-			}
-		}
-
-		// Undo the enchantments
-		for (const card of owner.hand) {
-			card.removeEnchantment("-1 cost", self);
-		}
 	},
 
 	async test(owner, self) {
-		// TODO: Add proper tests. #325
-		return true;
+		owner.mana = 5;
+		await self.trigger(Ability.Cast);
+
+		// The game hasn't ticked yet
+		assert.equal(owner.mana, 5);
+
+		// Manually tick the game
+		await game.event.tick(Event.GameLoop, -1, owner);
+
+		assert.equal(owner.mana, 10);
+
+		// Play a card to verify that the mana doesn't decrease
+		const card = await Card.create(game.cardIds.sheep1, owner);
+		const result = await game.play(card, owner);
+
+		assert.equal(result, GamePlayCardReturn.Success);
+		assert.equal(owner.mana, 10);
 	},
 };
