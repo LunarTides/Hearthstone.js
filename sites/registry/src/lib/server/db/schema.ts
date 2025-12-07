ee2e31f5e5bf9bf711b92b54884dacce34968f7c
LunarTides <31688109+LunarTides@users.noreply.github.com>
2025-12-07 09:33:42 +0100

    Add pack and card schemas

--- sites/registry/src/lib/server/db/schema.ts
+++ sites/registry/src/lib/server/db/schema.ts
@@ -1,4 +1,5 @@
-import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { boolean, integer, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
 
 export const user = pgTable("user", {
 	id: text("id").primaryKey(),
@@ -19,11 +20,83 @@ export const session = pgTable("session", {
 });
 
 export const pack = pgTable("pack", {
-	uuid: uuid("id").primaryKey(),
+	uuid: uuid("uuid").primaryKey(),
 	userId: text("user_id")
 		.notNull()
-		.references(() => user.id),
+		.references(() => user.id, { onDelete: "set default" }),
+	metadataVersion: integer("metadata_version").notNull(),
+	gameVersion: text("game_version").notNull(),
+	packVersion: text("pack_version").notNull(),
+	name: text("name").notNull().unique(),
+	description: text("description").notNull(),
+	license: text("license").notNull(),
+	authors: text("authors").array().notNull(),
+	// links: uuid("links").array().references(() => packLink.id),
+	// TODO: Add requires.
+
+	approved: boolean().notNull(),
 });
+
+export const packRelations = relations(pack, ({ many }) => ({
+	links: many(packLink),
+	cards: many(card),
+}));
+
+export const packLink = pgTable("packLinks", {
+	id: uuid("id").defaultRandom().primaryKey(),
+	packUUID: uuid("pack_uuid").references(() => pack.uuid, { onDelete: "cascade" }),
+	key: text("key").notNull(),
+	value: text("value").notNull(),
+});
+
+export const packLinkRelations = relations(packLink, ({ one }) => ({
+	pack: one(pack, {
+		fields: [packLink.packUUID],
+		references: [pack.uuid],
+	}),
+}));
+
+export const card = pgTable("card", {
+	uuid: uuid("id").primaryKey(),
+
+	packUUID: uuid("pack_uuid")
+		.references(() => pack.uuid)
+		.notNull(),
+
+	name: text("name").notNull(),
+	text: text("text").notNull(),
+	cost: integer("cost").notNull(),
+	type: text("type").notNull(),
+	classes: text("classes").array().notNull(),
+	rarity: text("rarity").notNull(),
+	tags: text("tags").array().notNull(),
+	collectible: boolean("collectible").notNull(),
+
+	attack: integer("attack"),
+	health: integer("health"),
+	tribes: text("tribes").array(),
+
+	spellSchools: text("spellSchools").array(),
+
+	durability: integer("durability"),
+	cooldown: integer("cooldown"),
+
+	armor: integer("armor"),
+	heropowerId: uuid("heropowerId"),
+
+	enchantmentPriority: integer("enchantment_priority"),
+});
+
+export const cardRelations = relations(card, ({ one }) => ({
+	heropower: one(card, {
+		fields: [card.heropowerId],
+		references: [card.uuid],
+	}),
+	pack: one(pack, {
+		fields: [card.packUUID],
+		references: [pack.uuid],
+	}),
+}));
 
 export type Session = typeof session.$inferSelect;
 
--- sites/registry/src/routes/upload/+page.server.ts
+++ sites/registry/src/routes/upload/+page.server.ts
@@ -4,6 +4,7 @@ import seven from "7zip-min";
 import { fileTypeFromBuffer } from "file-type";
 import { join, resolve } from "path";
 import { tmpdir } from "os";
+import { m } from "$lib/paraglide/messages.js";
 
 export const actions = {
 	default: async (event) => {
@@ -18,12 +19,12 @@ export const actions = {
 		}
 
 		if (file.type !== "application/x-7z-compressed") {
-			error(415, { message: "Invalid file type." });
+			error(415, { message: m.invalid_file_type() });
 		}
 
 		// TODO: Move to settings.
 		if (file.size > 100 * 1024 * 1024) {
-			error(413, { message: "Upload too large." });
+			error(413, { message: m.upload_too_large() });
 		}
 
 		const bytes = await file.bytes();
@@ -34,13 +35,13 @@ export const actions = {
 
 		// 7z magic byte.
 		if (magic !== "37 7A BC AF 27 1C") {
-			error(415, { message: "Invalid file type." });
+			error(415, { message: m.invalid_file_type() });
 		}
 
 		// Double check.
 		const ft = await fileTypeFromBuffer(bytes);
 		if (ft === undefined || ft.ext !== "7z" || ft.mime !== "application/x-7z-compressed") {
-			error(415, { message: "Invalid file type." });
+			error(415, { message: m.invalid_file_type() });
 		}
 
 		// Isolate temporarily.
@@ -51,13 +52,13 @@ export const actions = {
 		const files = await seven.list(tmpPackPath);
 		// TODO: Move to settings.
 		if (files.length > 5000) {
-			error(413, { message: "Too many files in archive." });
+			error(413, { message: m.archive_too_many_files() });
 		}
 
 		const uncompressedSize = files.map((f) => parseInt(f.size, 10)).reduce((p, c) => p + c, 0);
 		// TODO: Move to settings.
 		if (uncompressedSize > 100 * 1024 * 1024) {
-			error(413, { message: "Upload too large." });
+			error(413, { message: m.upload_too_large() });
 		}
 
 		// TODO: Move to settings.
@@ -70,12 +71,12 @@ export const actions = {
 			const target = resolve(tmpDirPath, file.name);
 			if (!target.startsWith(tmpDirPath)) {
 				console.log(1);
-				error(400, { message: "Archive invalid." });
+				error(400, { message: m.archive_invalid() });
 			}
 
 			// Allow directories.
 			if (!allowedExtensions.some((ext) => file.name.endsWith(ext)) && /\../.test(file.name)) {
-				error(400, { message: "Archive contains illegal file types." });
+				error(400, { message: m.archive_illegal_file_types() });
 			}
 
 			if (file.name.endsWith("meta.jsonc")) {
@@ -84,7 +85,7 @@ export const actions = {
 		}
 
 		if (!hasMeta) {
-			error(400, { message: "'meta.jsonc' not found." });
+			error(400, { message: m.meta_not_found() });
 		}
 
 		// TODO: This could maybe create symlinks, which would be bad?
@@ -99,13 +100,13 @@ export const actions = {
 			const folderStats = await fs.stat(packPath);
 			if (!folderStats.isDirectory()) {
 				console.log(2);
-				error(400, { message: "Archive invalid." });
+				error(400, { message: m.archive_invalid() });
 			}
 		} catch (err: any) {
 			// Folder not found.
 			if (err?.code === "ENOENT") {
 				console.log(3);
-				error(400, { message: "Archive invalid." });
+				error(400, { message: m.archive_invalid() });
 			}
 		}
 
@@ -115,7 +116,5 @@ export const actions = {
 
 		await fs.cp(packPath, finalPath, { recursive: true });
 		await fs.rm(packPath, { recursive: true, force: true });
-
-		// TODO: Copy metadata to metadatas folder.
 	},
 };
