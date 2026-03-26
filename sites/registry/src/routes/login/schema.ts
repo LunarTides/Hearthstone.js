b16b3260c0f66e7576d4908407ada39db33a6e0b
LunarTides <31688109+LunarTides@users.noreply.github.com>
2026-03-26 18:26:31 +0100

    Escape the `-` in login regex

--- sites/registry/src/routes/login/schema.ts
+++ sites/registry/src/routes/login/schema.ts
@@ -5,6 +5,9 @@ export default z.object({
 		.string()
 		.min(3)
 		.max(31)
-		.regex(/^[a-zA-Z0-9_-]+$/),
+		// NOTE: The minus has to be escaped to be used in HTML `input`,
+		// otherwise it spams the console with errors.
+		// eslint-disable-next-line no-useless-escape
+		.regex(/^[a-zA-Z0-9_\-]+$/),
 	password: z.string().min(6).max(255),
 });
